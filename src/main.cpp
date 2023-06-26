d811f8d07cb55b09ca0e23443a7834d3ce361fc6
Arthur Cadore <74879952+arthurcadore@users.noreply.github.com>
2023-06-26 11:30:47 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,20 +14,17 @@ This code is responsible for the user interface and the call of the other functi
 
 int main(int argc, char const *argv[]) {
 
+    // Importa os dados de processamento recebidos via linha de comando: 
+
+    // Arquivo de leitura (dataSet)
     string dataSetArchive = argv[1];
+    
+    // Estado a ser processado (sigla com dois digitos maiusculos): 
     string atualState = argv[2];
+
+    // Imprimir em formato CSV ou não: 
     bool printCSV = true;
-
-    // string dataSetArchive = INPUT1;
-    // string atualState = INPUT2;
-    // bool printCSV = true;
-
     
     // Faz a leitura do dataset linha a linha, utilizando como filtro o estado passado como argumento de linha de comando. 
     statesReader(dataSetArchive, atualState, printCSV);
 }
-
-// slide de correlação pra casos confirmados e mortes 
-// corelação de população e mortes 
-// área em (m2) de cada estado. 
-// densidade populacional com desvio padrão de cada estado. 
